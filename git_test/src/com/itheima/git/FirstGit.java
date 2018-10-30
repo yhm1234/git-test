3e3e803e23ac068d376b2143df3e05e5c96bb650
yanghongmei <2969389220@qq.com>
2018-10-30 19:33:07 +0800

    第二次修改FirstGit

--- git_test/src/com/itheima/git/FirstGit.java
+++ git_test/src/com/itheima/git/FirstGit.java
@@ -3,10 +3,12 @@ package com.itheima.git;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("git test");
+        //hello test
     }
 
     public void test() {
         int i = 1;
         i += 10;
+        System.out.println(i);
     }
 }
