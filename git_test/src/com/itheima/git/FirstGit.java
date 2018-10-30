97bb8518dab1252d67c86c525b1a82fb8c11f54f
yanghongmei <2969389220@qq.com>
2018-10-30 18:48:05 +0800

    修改FirstClass

--- git_test/src/com/itheima/git/FirstGit.java
+++ git_test/src/com/itheima/git/FirstGit.java
@@ -4,4 +4,9 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("git test");
     }
+
+    public void test() {
+        int i = 1;
+        i += 10;
+    }
 }
