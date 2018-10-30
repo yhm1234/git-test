9c306ccc598510a8171919087a9d7d51dcd1aed0
yanghongmei <2969389220@qq.com>
2018-10-30 18:37:00 +0800

    qq

--- git_test/src/com/itheima/git/FirstGit.java
+++
@@ -1,9 +0,0 @@
-package com.itheima.git;
-
-public class FirstGit {
-    public static void main(String[] args) {
-        System.out.println("git test");
-        System.out.println("git test");
-        System.out.println("git test");
-    }
-}
